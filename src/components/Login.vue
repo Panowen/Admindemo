40304fcbb727afa90576f7eb70da8d3f4a7f2dfe
powen <578080843@qq.com>
2017-09-15 15:58:06 +0800

    new

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    Login
+  </div>
+</template>
+<script>
+
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components:{
+
+    },
+    methods: {
+
+    }
+  }
+
+</script>
